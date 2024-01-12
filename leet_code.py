2ce7c2785ef4566ed0c39587d0108ef0bb286a0e
Anas Pawaskar <pawaskaranas10@gmail.com>
2024-01-12 19:39:46 +0530

    to reach target from the list

---
+++ leet_code.py
@@ -0,0 +1,18 @@
+def func(num,target):
+    for number in range(len(num)):
+        for number2 in range(number +1 , len(num)):
+            if num[number] + num[number2] == target:
+                print(number, number2)
+            
+# nums1 = [2, 7, 11, 15]
+# target = 9
+# print(func(num= nums1 , target= target))
+
+# nums2 = [8,5,4]
+# target2 = 12
+# print(func(num= nums2 , target= target2))
+
+# num3 = [1,2,1,1,1]
+# target3 = 3
+# print(func(num=num3,target = target3))
+
